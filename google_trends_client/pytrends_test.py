d9ff6c60edae1fb778baea042562836fe4c494f2
iMurfyD <imdesjardin@gmail.com>
2017-11-05 02:13:05 -0500

    Generates geographic info

--- google_trends_client/pytrends_test.py
+++ google_trends_client/pytrends_test.py
@@ -2,20 +2,28 @@
 from pytrends.request import TrendReq
 import json, os, sys
 
-def find_geos(tf, phrases):
+def find_geos(tf, phrase):
 	pytrends = TrendReq(hl='en-US', tz=360)
-	pytrends.build_payload(phrases, cat=0, timeframe='now 7-d', geo='', gprop='')
+	pytrends.build_payload([phrase], cat=0, timeframe='now 7-d', geo='', gprop='')
 	print(type(pytrends.interest_by_region(resolution='COUNTRY')))
 	return pytrends.interest_by_region(resolution='COUNTRY')
 
 def find_and_dump_geos(tf, phrases):
-    geos = find_geos(tf,phrases)
-    data = geos.to_json()
-    fh = open('data.json', 'w')
-    fh.write(data)
+    print(phrases)
+    print(tf)
+    fh = open('data.json', 'a')
+    for phrase in phrases:
+        print(phrase)
+        if len(phrase) < 20:
+            try:
+                geos = find_geos(tf,phrase)
+                data = geos.to_json()
+                fh.write(data)
+            except:
+                print("Fucked up")
     fh.close() 
-    return geos
+    return 1
 
 if __name__ == '__main__':
-	kw_list = ['Normie Memes', 'reposts']
+	kw_list = ['Normie Memes', 'reposts', 'weirdest thing done sexually', 'cat face against floor']
 	print(find_and_dump_geos('now 7-d', kw_list))
--- meme_engine.py
+++ meme_engine.py
@@ -1,14 +1,10 @@
 #! /usr/bin/env python3
 from imgur_client import fetch_popular_memes as imgur
 from mrisa_interface import identify_image as mrisa
+from google_trends_client import pytrends_test as geo
 import json
 
 if __name__ == '__main__':
     memes = imgur.top_10('week')
     ids = mrisa.get_names(memes)
-    print(ids)
-    print('Successfully found ', len(ids), ' memes')
-    outfile = open('dank_memes.json', 'w')
-    json.dump(ids, outfile)
-    outfile.close()    
-
+    geo.find_and_dump_geos('now 7-d', ids)  
